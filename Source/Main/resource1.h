c9f231bb1915fa064522181687a1081055b77378
chengr28 <chengr28@gmail.com>
2013-04-23 01:18:03 +0800

    Building Project

---
+++ Source/Main/resource1.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by antiLeech.rc
+//
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+#ifndef _T
+#define _T(x)	x
+#endif
+
+#define _chSTR(x)		_T(#x)
+#define chSTR(x)		_chSTR(x)
+
